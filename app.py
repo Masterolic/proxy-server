9873cc9a07950d5135215d40a31f73c9429ef264
Masterolic <rockingmallu8@gmail.com>
2024-04-15 20:41:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, Response
-import requests
+import requests, os
 
 app = Flask(__name__)
 
@@ -19,4 +19,5 @@ def proxy():
         return f'Error: {e}', 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port  = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
